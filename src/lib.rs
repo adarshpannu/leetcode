ed034ba1c50a9aa0ab62526d97bbcda4941f8bb9
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-06-04 13:28:44 -0700

    IntoIter on single-linked list

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,25 @@ struct Node<T> {
     next: Link<T>,
 }
 
+struct ListIntoIter<T> {
+    list:   List<T>
+}
+
+impl<T> Iterator for ListIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop()        
+    }
+}
+
 impl<T> List<T> {
     fn new() -> Self {
         List { head: None }
+    }
+
+    fn into_iter(self) -> ListIntoIter<T> {
+        ListIntoIter {list: self}
     }
 
     fn push(&mut self, elem: T) {
@@ -55,5 +71,19 @@ mod tests {
 
         assert_eq!(None, lst.pop());
         assert_eq!(None, lst.peek());
+    }
+
+    #[test]
+    fn test_into_iter() {
+        let mut lst = crate::List::new();
+
+        lst.push(23);
+        lst.push(57);
+
+        let mut iter = lst.into_iter();
+        assert_eq!(iter.next(), Some(57));
+        assert_eq!(iter.next(), Some(23));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
     }
 }
