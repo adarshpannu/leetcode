7951571dc2b7c4ad3f6fae77b48faa808ec15af8
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-06-24 12:33:40 -0700

    atoi2 complete.

--- src/atoi2.rs
+++ src/atoi2.rs
@@ -9,9 +9,12 @@ struct Solution {}
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 enum State {
     Start,
+    Base0,
     Base,
+    Fractional0,
     Fractional,
     Exponent,
+    ExponentBase0,
     ExponentBase,
     End,
 }
@@ -23,6 +26,7 @@ enum Token {
     Digit,
     E,
     Dot,
+    Unknown,
     EOF,
 }
 use Token::*;
@@ -43,16 +47,23 @@ struct NumberParser {
 impl NumberParser {
     fn new(str: String) -> NumberParser {
         let transitions = vec![
-            ((Start, Sign), Base),
+            ((Start, Sign), Base0),
             ((Start, Digit), Base),
-            ((Start, Dot), Fractional),
+            ((Start, Dot), Fractional0),
+            ((Base0, Digit), Base),
+            ((Base0, Dot), Fractional0),
             ((Base, Digit), Base),
-            ((Base, Dot), Fractional),
+            ((Base, E), Exponent),
+            ((Base, Dot), Fractional0),
             ((Base, EOF), End),
+            ((Fractional0, Digit), Fractional),
+            ((Fractional0, EOF), End),
+            ((Fractional0, E), Exponent),
             ((Fractional, Digit), Fractional),
             ((Fractional, E), Exponent),
             ((Fractional, EOF), End),
-            ((Exponent, Sign), ExponentBase),
+            ((Exponent, Sign), ExponentBase0),
+            ((ExponentBase0, Digit), ExponentBase),
             ((Exponent, Digit), ExponentBase),
             ((ExponentBase, Digit), ExponentBase),
             ((ExponentBase, EOF), End),
@@ -71,10 +82,10 @@ impl NumberParser {
             self.next_idx += 1;
             match ch {
                 '+' | '-' => Token::Sign,
-                '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => Token::Digit,
+                '0'..='9' => Token::Digit,
                 '.' => Token::Dot,
                 'e' | 'E' => Token::E,
-                _ => panic!("Illegal character found: {}", ch),
+                _ => Token::Unknown,
             }
         }
     }
@@ -82,17 +93,28 @@ impl NumberParser {
     fn parse(&mut self) -> Result<(), &'static str> {
         let mut token;
         let mut state = Start;
-    
+        let mut seen_base = false;
+        let mut seen_fractional = false;
+
         loop {
             token = self.next_token();
+            if token == Token::Unknown {
+                return Err("Unexpected character/token found.");
+            }
+
             let key = (state, token);
             let new_state = self.transitions.get(&key);
             if new_state.is_none() {
                 return Err("Illegal state transition.");
             }
             let new_state = new_state.unwrap();
-            println!("({:?}, {:?}) -> {:?}", state, token, new_state);
+            //println!("({:?}, {:?}) -> {:?}", state, token, new_state);
             state = *new_state;
+            match state {
+                Base => seen_base = true,
+                Fractional => seen_fractional = true,
+                _ => {}
+            }
             if token == Token::EOF {
                 break; 
             }
@@ -100,11 +122,14 @@ impl NumberParser {
         if state != End {
             return Err("Unexpected end of input.");
         }
+        if !seen_base && !seen_fractional {
+            return Err("Malforned number: neither base nor fractional part exists.");
+        }
         Ok(())
     }    
 }
 
 #[test]
 fn run() {
-    println!("\nRESULT = {:?}\n", Solution::is_number(".e9".to_string()));
+    println!("\nRESULT = {:?}\n", Solution::is_number("46.e3".to_string()));
 }
