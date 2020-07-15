0c7bf839aca96666c8b12e38dc84c6af8624da96
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-07-14 23:22:58 -0700

    lc780_reaching_points

--- src/lc780_reaching_points.rs
+++ src/lc780_reaching_points.rs
@@ -1,25 +1,50 @@
 use std::collections::VecDeque;
 
+fn is_even(i: i32) -> bool {
+    i % 2 == 0
+}
+
+fn is_odd(i: i32) -> bool {
+    !is_even(i)
+}
+
 impl Solution {
     pub fn reaching_points(sx: i32, sy: i32, tx: i32, ty: i32) -> bool {
         let mut q: VecDeque<(i32, i32)> = VecDeque::new();
+        let mut n_considered = 0;
+
         q.push_front((sx, sy));
         while let Some((sx, sy)) = q.pop_front() {
-            //println!("Candidate: ({}, {})", sx, sy);
+            n_considered += 1;
+            println!("Candidate: ({}, {})", sx, sy);
+
             if (sx, sy) == (tx, ty) {
+                dbg!(n_considered);
                 return true;
             }
             for (ex, ey) in Self::transform(sx, sy).iter() {
-                if (*ex <= tx) && (*ey <= ty) && (*ex + *ey <= tx + ty) {
+                if !Self::prune(*ex, *ey, tx, ty) {
                     q.push_back((*ex, *ey));
                 }
             }
         }
+        dbg!(n_considered);
         false
     }
 
     fn transform(sx: i32, sy: i32) -> [(i32, i32); 2] {
         [(sx, (sx + sy)), ((sx + sy), sy)]
+    }
+
+    fn prune(sx: i32, sy: i32, tx: i32, ty: i32) -> bool {
+        if is_even(sx) && is_even(sy) && (is_odd(tx) || is_odd(ty)) {
+            return true;
+        }
+        if (sx <= tx) && (sy <= ty) && (sx + sy <= tx + ty) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
@@ -29,6 +54,6 @@ struct Solution;
 fn test() {
     //assert_eq!(Solution::reaching_points(1, 1, 3, 5), true);
     //assert_eq!(Solution::reaching_points(1, 1, 1, 1), true);
-    assert_eq!(Solution::reaching_points(35, 13, 455955547, 420098884), true);
-
+    //assert_eq!(Solution::reaching_points(35, 13, 455955547, 420098884), true);
+    assert_eq!(Solution::reaching_points(1, 1, 998, 999), true);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(warnings)]
 
-mod lc780_reaching_points;
+mod lc780_reaching_points; // in progress, need to work on pruning search space
 
 fn main() {
     println!("Hello, world");
