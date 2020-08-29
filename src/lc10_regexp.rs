f18a1ff8a9a85fc03930e7439ececef3a358b506
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-08-28 17:13:28 -0700

    Back!

---
+++ src/lc10_regexp.rs
@@ -0,0 +1,46 @@
+/*
+Given an input string (s) and a pattern (p), implement regular expression 
+matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+
+
+c*a*b* matches aab
+*/
+
+/* State Transitions: (State,Event) -> State 
+
+Start + 'c' -> 
+Start + 'a' -> 
+
+*/
+
+enum Event {
+    Dot,
+    Star(char),
+    Char(char),
+}
+
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        unimplemented!()
+    }
+
+    pub fn compile(re: String) {
+        for e in re.chars() {
+
+        }
+    }
+}
+
+struct Solution;
+
+#[test]
+fn test() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #[allow(warnings)]
 
 //mod lc780_reaching_points; // in progress, need to work on pruning search space
-mod lc1512_good_pairs;
+//mod lc1512_good_pairs;
+
+mod lc10_regexp;
 
 fn main() {
     println!("Hello, world");
 }
+
