bae972ef90399917d7da34009e7bceb9372bda84
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-11-24 19:27:10 -0800

    Checkin

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 //mod lc780_reaching_points; // in progress, need to work on pruning search space
 //mod lc1512_good_pairs;
 
-mod lc_binary_to_dec;
+mod lc283_move_zeros;
 
 fn main() {
     println!("Hello, world!");
