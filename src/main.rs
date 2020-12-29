c417b6473bda252c750fdfa472f6d39f8589be79
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2020-12-29 00:49:42 -0800

    More

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ mod lc564_closest_palindrome;
 
 fn main() {
     println!("Hello, world!");
+    dbg!(first_word(&"Hello world".to_string()));
+    dbg!(first_word(&" Hello ".to_string()));
 }
 
+
+fn first_word(s: &String) -> &str {
+    return match s.find(' ') {
+        Some(i) => &s[0..i],
+        None => &s[..]
+    }
+}
 
