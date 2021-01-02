b3bdfcf7dec3ce9e79fc956c46967b361d47479d
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-01-01 22:29:24 -0800

    Testing group_by

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,15 @@ mod lc564_closest_palindrome;
 fn main() {
     println!("Hello, world!");
 }
+
+use itertools::Itertools;
+
+/* */
+#[test]
+fn test() {
+    let l = vec![3, 3, 3, 2, 1, 3, 2];
+
+    let x = l.iter().zip(0..l.len()).group_by(key)
+}
+
+*/
