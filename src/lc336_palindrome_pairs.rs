d3ef92df3f1ef4d62565dc742487262233109c05
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-01-15 23:48:39 -0800

    WIP

--- src/lc336_palindrome_pairs.rs
+++ src/lc336_palindrome_pairs.rs
@@ -1,22 +1,28 @@
 struct Solution;
 
 impl Solution {
-    fn is_palindrome(str: String) -> bool {
-        let chars: Vec<char> = str.chars().collect();
-        chars
-            .iter()
-            .zip(chars.iter().rev())
-            .all(|(ch1, ch2)| *ch1 == *ch2)
+    fn is_palindrome(word1: &Vec<char>, word2: &Vec<char>) -> bool {
+        let len = word1.len() + word2.len();
+        let mid = len / 2 as usize;
+
+        let get_char = |ix: usize| {
+            if ix < word1.len() {
+                word1[ix]
+            } else {
+                word2[ix - word1.len()]
+            }
+        };
+        (0..mid).all(|ix| get_char(ix) == get_char(len - 1 - ix))
     }
 
     pub fn palindrome_pairs(words: Vec<String>) -> Vec<Vec<i32>> {
         let mut retval: Vec<Vec<i32>> = Vec::new();
+        let words: Vec<Vec<char>> = words.iter().map(|e| e.chars().collect()).collect();
 
         for (ix, word1) in words.iter().enumerate() {
             for (jx, word2) in words.iter().enumerate() {
                 if ix != jx {
-                    let word = format!("{}{}", word1, word2);
-                    if Self::is_palindrome(word) {
+                    if Self::is_palindrome(word1, word2) {
                         let v = vec![ix as i32, jx as i32];
                         retval.push(v);
                     }
@@ -34,6 +40,4 @@ fn test() {
     let words = vec!["abcd", "dcba", "lls", "s", "sssll"];
     let words = words.iter().map(|e| e.to_string()).collect();
     dbg!(Solution::palindrome_pairs(words));
-
-    dbg!(Solution::is_palindrome("abba".to_owned()));
 }
